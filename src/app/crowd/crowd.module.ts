891afb28a659c1f44199ddc52d2b859a2508b88c
tarak.cdtp <Beskyzer@007a>
2020-05-14 22:58:43 +0100

    templ v1

--- src/app/crowd/crowd.module.ts
+++ src/app/crowd/crowd.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CrowdRoutingModule } from './crowd-routing.module';
 import { FrontComponent } from './templates/front/front.component';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   declarations: [FrontComponent],
   imports: [
     CommonModule,
-    CrowdRoutingModule
+    CrowdRoutingModule,
+    SharedModule
   ]
 })
 export class CrowdModule { }
